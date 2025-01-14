bc177926eda4ccc4bfe8dd59b123d1e18b917a08
Cihan Andac <cihanandac@gmail.com>
2025-01-14 10:31:23 +0100

    feat: remove timerly

--- config.lua
+++ config.lua
@@ -292,20 +292,6 @@ lvim.plugins = {
       }
     end,
   },
-
-  { "nvzone/timerly", 
-    dependencies = {
-      "nvzone/volt",
-    },
-    config = {
-      minutes = { 25, 5 },
-      on_finish = function()
-        vim.notify "Timerly: time's up!"
-      end,
-      position = "top-right", -- top-left, top-right, bottom-left, bottom-right
-      -- or function(w, h) return row, col end , w - w of window, arg passed by plugin
-    },
-  },
 }
 
 -- Theme choice
